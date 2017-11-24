e9f503b82411ce68499f241fee904914a5dcc0a6
Pauline JACOB <pauline.lille@gmail.com>
2017-11-24 16:46:43 +0100

    install bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/*const appRoutes: Routes = [
+  { path: 'classique', component: ClassiqueComponent }, // /page1 affiche le composant A
+  { path: '**', redirectTo: 'classique'} // redirige vers la route page1 par défaut
+];*/
 
 @NgModule({
   declarations: [
