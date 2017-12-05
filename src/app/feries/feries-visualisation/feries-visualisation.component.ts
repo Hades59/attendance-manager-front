530498ee8910cd08634b19ceff204f381287f9c9
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-12-05 10:34:40 +0100

    ferie changements

--- src/app/feries/feries-visualisation/feries-visualisation.component.ts
+++ src/app/feries/feries-visualisation/feries-visualisation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons,NgbActiveModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common';
+import { Feries } from '../../shared/domain/feries';
 
 @Component({
   selector: 'app-feries-visualisation',
@@ -7,6 +9,8 @@ import {NgbModal, ModalDismissReasons,NgbActiveModal, NgbModalRef} from '@ng-boo
   styleUrls: ['./feries-visualisation.component.css']
 })
 export class FeriesVisualisationComponent implements OnInit {
+
+  feries: Feries[]
 
   protected currentModal:NgbModalRef
   
