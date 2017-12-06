ca3c800ab3fc93de1f48d3c8fd976443f49f7dbb
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-12-06 11:27:05 +0100

    modification css ferie - gestion absence

---
+++ src/app/feries/feries-update/feries-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feries-update',
+  templateUrl: './feries-update.component.html',
+  styleUrls: ['./feries-update.component.css']
+})
+export class FeriesUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
