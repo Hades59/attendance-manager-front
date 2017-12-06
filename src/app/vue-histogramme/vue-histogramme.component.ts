adf7229c3d6de6be9d09b6ac71ed03fd8c3f4853
Pauline JACOB <pauline.lille@gmail.com>
2017-12-06 16:53:16 +0100

    changement histo

--- src/app/vue-histogramme/vue-histogramme.component.ts
+++ src/app/vue-histogramme/vue-histogramme.component.ts
@@ -8,6 +8,27 @@ import { startOfDay,
   isSameMonth,
   addHours } from 'date-fns';
 import { DatePipe } from '@angular/common';
+import { AbsenceService } from '../shared/service/absence.service';
+import { Subject } from 'rxjs/Subject';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  },
+  purple: {
+    primary: '##ad2095',
+    secondary: '#ff2bda'
+  }
+ };
 
 @Component({
   selector: 'app-vue-histogramme',
@@ -20,7 +41,36 @@ export class VueHistogrammeComponent implements OnInit {
   moisTab: Date[] = []
   viewDate: Date = new Date();
 
-  constructor() { }
+
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+ 
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40]},
+    {data: [28, 48, 40, 19, 86, 27, 90]}
+  ];
+
+  constructor(public absenceService: AbsenceService) { 
+    this.absenceService.listerAbsenceParStatus("VALIDEE")
+                        .subscribe(absences => absences.forEach(abs => {
+      
+            let color : any = colors.purple;
+            if (abs.type=='RTT'){
+              color=colors.yellow;
+            }
+            if (abs.type=='CONGE_PAYE'){
+              color=colors.blue;
+            } 
+            if (abs.type=='CONGE_SANS_SOLDE'){
+              color=colors.red;
+            }
+          }))                  
+   }
 
   ngOnInit() {
     let today: Date = new Date();
@@ -34,20 +84,10 @@ export class VueHistogrammeComponent implements OnInit {
       this.moisTab.push(new Date(annee, i));
    
     }
+    console.log(this.moisTab)
+    this.barChartLabels = this.moisTab.map(m => ''+m.getDay());
   }
 
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartLabels:string[] = [];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
- 
-  public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40]},
-    {data: [28, 48, 40, 19, 86, 27, 90]}
-  ];
  
   // events
   public chartClicked(e:any):void {
